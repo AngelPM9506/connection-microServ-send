a45ab39e65a579e4e55cf6b8e2f0aea631cbfb5d
Miguel Angel Parra Mondragon <parra_mondragon@live.com>
2024-07-28 22:46:58 -0600

    Se agrega compatibilidad con php5.6

---
+++ src/NDD_send_connect_php56.php
@@ -0,0 +1,113 @@
+<?php
+
+namespace NDD_send_connect_php56;
+
+class NDD_send_connect_php56
+{
+    private $defultUrl = 'http://send.test01.app';
+    private $url = '';
+    private $apikey;
+    private $defaultheaders = [
+        'Accept: application/json',
+        'Content-Type: application/json',
+    ];
+    private $headers = [
+        'Accept: application/json',
+        'Content-Type: application/json',
+        'Authorization: '
+    ];
+    private $isToken = false;
+
+    public function __construct($apikey, $url = null, $isToken = false)
+    {
+        if (!$apikey) {
+            throw new \Exception("Apikey is required", 1);
+        }
+        $this->apikey = $apikey;
+        $this->url = $url ? $url : $this->defultUrl;
+        $this->headers[] = 'Authorization: ' . $this->apikey;
+        $this->isToken = $isToken;
+    }
+
+    public function set_url($url)
+    {
+        if (!$url) {
+            throw new \Exception("Url is required", 1);
+        }
+        $this->url = $url;
+    }
+
+    public function set_defaultUrl()
+    {
+        $this->url = $this->defultUrl;
+    }
+
+    public function get_url()
+    {
+        return $this->url;
+    }
+
+    public function set_apikey($apikey, $isToken = false)
+    {
+        if (!$apikey) {
+            throw new \Exception("Apikey is required", 1);
+        }
+        $this->apikey = $apikey;
+        $this->headers = [
+            'Accept: application/json',
+            'Content-Type: application/json',
+            'Authorization: ' . $this->apikey
+        ];
+    }
+
+    private function send_request($endpoint, $data)
+    {
+        $ch = curl_init($this->url . $endpoint);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, $this->headers);
+        curl_setopt($ch, CURLOPT_POST, true);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($data));
+        $response = curl_exec($ch);
+        if (curl_errno($ch)) {
+            throw new \Exception(curl_error($ch));
+        }
+        curl_close($ch);
+        return json_decode($response, true);
+    }
+
+    public function send_email($from, $to, $subject, $cc = null, $bcc = null, $html = null, $text = null)
+    {
+        if (!$to || !$html || !$subject) {
+            throw new \Exception("To, Html and Subject are required", 1);
+        }
+        $data = [
+            'from' => $from,
+            'to' => $to,
+            'subject' => $subject,
+            'cc' => $cc,
+            'bcc' => $bcc,
+            'html' => $html,
+            'text' => $text
+        ];
+
+        return $this->send_request('/v1/send/key', $data);
+    }
+
+    public function send_email_token($from, $to, $subject, $cc = null, $bcc = null, $html = null, $text = null)
+    {
+        if (!$to || !$html || !$subject) {
+            throw new \Exception("To, Html and Subject are required", 1);
+        }
+        $data = [
+            'from' => $from,
+            'to' => $to,
+            'subject' => $subject,
+            'cc' => $cc,
+            'bcc' => $bcc,
+            'html' => $html,
+            'text' => $text
+        ];
+
+        return $this->send_request('/v1/send/token', $data);
+    }
+}
--- test.php
+++ test.php
@@ -2,9 +2,30 @@
 require __DIR__ . '/vendor/autoload.php';
 
 use NDD_send_connect\NDD_send_connect;
+use NDD_send_connect_php56\NDD_send_connect_php56;
 
 
-$client = new NDD_send_connect('apikey');
+$client = new NDD_send_connect('apikey', 'https://send.test81.app');
+
+
+echo 'Testing 8.1';
+echo "<br>";
 
 echo $client->get_url();
-echo 'End Testing';
+echo "<br>";
+
+echo 'End Testing 8.1';
+echo "<br>";
+
+
+$client = new NDD_send_connect_php56('apikey' , 'https://send.test56.app');
+
+echo 'Testing 5.6';
+echo "<br>";
+
+echo $client->get_url();
+echo "<br>";
+
+echo 'End Testing 5.6';
+echo "<br>";
+
